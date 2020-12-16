b9406b7abb75cba690859c763fc0353b0b18d41d
Gary Swartz <garyswartz18@gmail.com>
2020-12-16 22:44:14 +0700

    finish card slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,67 @@
+// *==== Menu Navigation
 const menuBtn = document.querySelector(".menu-btn");
 const menuItems = document.querySelector(".menu-items");
-// const navbar = document.querySelector(".navbar");
 
 menuBtn.addEventListener("click", () => {
   toggle();
-});
-// navbar.addEventListener("scroll", () => {
-//   navbar.classList.toggle("sticky", window.scrollY > 0);
-// });
-
-window.addEventListener("scroll", function () {
-  var navbar = document.querySelector(".navbar");
-  navbar.classList.toggle("sticky", window.scrollY > 0);
 });
 
 function toggle() {
   menuBtn.classList.toggle("open");
   menuItems.classList.toggle("open");
 }
+
+// *==== Scroll to show shadow
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector(".navbar");
+  navbar.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// *==== Card Slider Manual/Auto.
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".slide-btn");
+var profile = document.querySelector(".customer-profile");
+
+let currentSlide = 0;
+
+// *== Manual Slider Navigation
+var manualNav = function (manual) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+
+    // profile.classList.add("active");
+
+    btns.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+  });
+
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", manualNav(currentSlide));
+
+const next = () => {
+  currentSlide >= slides.length - 1 ? (currentSlide = 0) : currentSlide++;
+  manualNav(currentSlide);
+};
+const pre = () => {
+  currentSlide <= 0 ? (currentSlide = slides.length - 1) : currentSlide--;
+  manualNav(currentSlide);
+};
+
+document.querySelector(".next").addEventListener("click", next);
+document.querySelector(".pre").addEventListener("click", pre);
+
+// *== Auto Slider per 10s.
+setInterval(() => {
+  next();
+}, 5000);
